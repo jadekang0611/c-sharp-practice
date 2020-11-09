8f25d09cc711b873f2d2bab10383f34811a0e320
jadekang <jkang.se@gmail.com>
2020-11-09 18:16:24 +0900

    Learned loop in c#

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ namespace GradeBook
         static void Main(string[] args)
         // args is the name of the parameter
         {   
+            var numbers = new [] {12.7, 10.3, 6.11};
+            
+
+            // var result = numbers[0];
+            // result = result + numbers[1];
+            // result = result + numbers[2];
+            // Loop
+
+            var result=0.0;
+            foreach (var item in numbers)
+            {
+                result+= item;
+            }
+            Console.WriteLine(result);
+
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
@@ -18,3 +34,12 @@ namespace GradeBook
         }
     }
 }
+
+/* Here is a given problem
+We need an electronic grade book to read the scores of an individual student and then compute some simple
+statistics from the scores
+
+The grades are entered as floating point numbers from 0 to 100, and the statistics should show us the highest grade,
+the lowest grade, and the average grade.
+*/
+
